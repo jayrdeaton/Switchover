da4ee493c1d36f2235362038fae002658c5099a8
jaydeaton <jayrdeaton@gmail.com>
2019-02-04 09:49:48 -0600

    added list of tags to extract

---
+++ src/actions/products/tagList.js
@@ -0,0 +1,15 @@
+module.exports = [
+  { input: 'HARDWARE', output: 'Hardware' },
+  { input: 'ELECTRONICS', output: 'Electronic' },
+  { input: 'SYSTEM PACK', output: 'System Pack' },
+  { input: 'CONTROLLERS', output: 'Controller' },
+  { input: 'CONTROLLER', output: 'Controller' },
+  { input: 'ACCESSORY', output: 'Accessory' },
+  { input: 'MP3 PLAYERS', output: 'MP3 Player' },
+  { input: 'UNIVERSAL', output: 'Universal' },
+  { input: 'CORD', output: 'Cord' },
+  { input: 'CORDS', output: 'Cord' },
+  { input: 'CABLE', output: 'Cord' },
+  { input: 'TABLETS', output: 'Tablet' },
+  { input: 'PHONE', output: 'Phone' }
+]
